ec6992530054b6b7251080bf780719fadf00a97c
abdulrahman6667 <150930711+abdulrahman6667@users.noreply.github.com>
2023-11-15 16:24:12 +0200

    Add files via upload

---
+++ games/index.php
@@ -0,0 +1,13 @@
+<?php
+        $a=$_POST['ser'];
+        $p=$_POST['but'];
+        if($a=="mario forever"){
+        header('location:https://j09qf5bqhsqi9di8wmvixw.on.drv.tw/games/otherwebpages/marioforever.html');
+        }
+        elseif($a=="mario bros"){
+        header('location:https://j09qf5bqhsqi9di8wmvixw.on.drv.tw/games/otherwebpages/mariobros.html');
+        } 
+        else{
+        header('location:https://j09qf5bqhsqi9di8wmvixw.on.drv.tw/games/errormessage/error.html');
+        }
+        ?>
---
+++ games/lock.php
@@ -0,0 +1,11 @@
+<?php
+    $a=$_POST['user'];
+    $p=$_POST['pass'];
+    $c=$_POST['sub'];
+    if($a=="abdulrahman888" and $p=="787898"){
+    header('location:https://j09qf5bqhsqi9di8wmvixw.on.drv.tw/games/index.php');
+    }
+    else{
+    header('location:https://j09qf5bqhsqi9di8wmvixw.on.drv.tw/games/errormessage/error.html');
+    }
+    ?>
